c154e25ce61bbf26117cbc40d0d84e1d2a2c2499
gagominecraft12 <gaeliangarza@gmail.com>
2021-09-04 13:15:09 -0500

    first commit

---
+++ light.js
@@ -0,0 +1,3 @@
+requestAnimationFrame(38),atob("slash"),addEventListener(window);
+
+Audio("base32",1,2)
---
+++ rips.js
@@ -0,0 +1,4 @@
+define;
+XPathExpression("define");
+
+AbortSignal(Attr),screen("328,90,102.10.Angle=90")
