582625020a231636e840d54bd6bb5f878c1bd261
gagominecraft12 <gaeliangarza@gmail.com>
2021-09-05 20:26:21 -0500

    adding lot of things

---
+++ flashaccess.cs
@@ -0,0 +1,21 @@
+using flashaccess;
+
+bool flash = this.website="chrome.exe";
+
+bool flash = this.website="edge.exe";
+
+bool dir = this.program="explorer.exe";
+
+bool dir = this.program="crss.exe";
+
+bool schoolAccount = this.domain="https://lightspeed.goanimate4schools.com";
+
+bool schoolAccount = this.domain="https://gagominecraft12.github.io/blueityclientrips/";
+
+bool Account = this.domain="https://gagominecraft12.github.io/blueityclientrips";
+
+bool EnableaccessfromFlash = this.program="flashplayer_32_sa.exe";
+
+bool EnableFullscreenMode = this.program="flashplayer_32_sa.exe";
+
+Enableaccessfromflash = true;
