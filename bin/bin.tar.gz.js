97eed9dac43e85af633bf1a4e3ac1a0f25502085
gagominecraft12 <gaeliangarza@gmail.com>
2021-09-28 15:52:47 -0500

    even more assets of a bit time LOL

---
+++ bin/bin.tar.gz.js
@@ -0,0 +1,24 @@
+/*
+  The Nuker 2 as a Friday Night Funkin Modchart 
+*/
+
+const bin = require("./bin");
+
+const tts = require("tts");
+
+const AcapelaTV = require("./bin.tar.gz");
+
+const utterlan = {
+    utterlan(nil);
+}
+
+let ttsAPI = new Promise();
+
+const nil = {
+    /*
+    To say:
+    I even do not own vyond. this origin has been accepted to people. we are gonna have lua code soon yoo lol
+    */
+
+    _____WB$wombat$assign$function_____
+}
