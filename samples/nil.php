2cbf57e4248a23f504a0a8d1b931fe1fc7b44e25
Blue'ity <65516039+gagominecraft12@users.noreply.github.com>
2021-10-27 22:22:00 -0500

    yes

--- samples/nil.php
+++ samples/nil.php
@@ -1,6 +1,12 @@
 <!DOCTYPE php>
 <title>test</title>
 
-<div id="sample">
-    <p>Nothing to find.</p>
-</div>
+<php lang="en" xmlns="">
+    <head>
+        <link href="../def.css" rel="stylesheet">
+    </head>
+
+    <header class="container">
+        <img src="./static/img/logo4s.png" width="420" height="60">
+    </header>
+</php>
