97ab28f3e48d29841f1a45b7d167962ba1b8168c
Blue'ity <65516039+gagominecraft12@users.noreply.github.com>
2021-10-30 18:36:30 -0500

    bored rn and updates took me a whole day to finish this

--- go_full.header.js
+++ go_full.header.js
@@ -3,7 +3,7 @@ let nil = getSelection("line: 4")
 
 const xml = 'XMLSelection' = function(req, res, url, http, nil, letscript){
     res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-	res.end(xml);
+	res.end()
 }
 
 
