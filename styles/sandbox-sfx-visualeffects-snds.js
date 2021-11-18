c683429f960ead1a31b91298913fc58fbd3176c4
Blue'ity <65516039+gagominecraft12@users.noreply.github.com>
2021-11-17 20:23:52 -0600

    yes (i think i added storyboard to make this  normal)

---
+++ styles/sandbox-sfx-visualeffects-snds.js
@@ -0,0 +1,25 @@
+var client = "https://github.com/gagominecraft12/blueityclientrips.git";
+if (client === window.fetch("https://github.com/gagominecraft12/blueityclientrips.git")) {
+} else { var a = true;
+    var blueitySandboxVersion = "1.0", a;
+    
+    alert("This Experiment is created by Blue'ity! Version: " + blueitySandboxVersion)
+    
+    function VisualEffects() {
+        module.exports = key["a", "1", 2];
+    
+        module.exports ; if (window.onclick == a) { a(alert(varErrorNext)) }
+    }
+    
+    function snds() {
+        module.exports = [a];
+    
+        if (onclick === alert('Are you sure to remove the sound?')) return removeSnd();
+    }
+    function removeSnd() {
+        snds(onclick(addEventListener(Map('<msg><body class="message"><p>Succesfully removed sound!</p></body></msg>'))));
+    }
+    function addSnd() {
+        snds(onclick(addEventListener(Map('<msg><body class="message"><p>Succesfully added</p></body></msg>'))));
+    }
+}
