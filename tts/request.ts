f75c1c5f6f170c13d2f4eea595f7188a45530b47
Blue'ity <65516039+gagominecraft12@users.noreply.github.com>
2021-11-26 23:29:04 -0600

    yes

---
+++ tts/request.ts
@@ -0,0 +1,18 @@
+const request = new Request();
+
+async function request() {
+    XMLHttpRequest.OPENED("GET", "/xml/request/tts.rss.xml");
+    
+    if (request.method === "GET" || XMLHttpRequest.HEADERS_RECEIVED === "text/typescript") {
+    } else {
+        try {
+            
+        } catch (tan) {
+            
+        }
+    }
+}
+
+while (request()) {
+    addEventListener("click".onclick(window.Function(addEventListener("abort"))));
+}-open();
