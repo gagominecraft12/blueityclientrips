a711c5fe39dda61bda3a6d637a7288eff39b899c
Blue'ity <65516039+gagominecraft12@users.noreply.github.com>
2021-12-09 19:36:42 -0600

    yes

--- styles/school/preview.js
+++ styles/school/preview.js
@@ -1,5 +1,7 @@
-var err = this.name = "err";
+var err = this.name = "err" + url.protocol("wss://");
 var url = new URL();
+var psuedo = addEventListener(".preview");
+var request = new Request();var preservernotfound = "Preview Server Cannot be found. Error code: ", Math.random();
 
 module.exports = function(x,a,c,i) {
     try {
@@ -10,3 +12,22 @@ module.exports = function(x,a,c,i) {
     }
 }
 events(1|1*1*4||4|1*1).content("preview");
+
+// Will fetch for content
+
+if (url.hostname !== "wss://" + "preview.vyond.com") {
+    fetch("/") + request.method !== "post";
+    if (request.destination !== "wss://preview.vyond.com") {
+    throw new Error = {
+        message(preservernotfound)
+    }
+
+    psuedo("content" + postMessage/name("content"))
+}
+} else {
+    request.headers = "text/javascript";
+    request.clone("/");
+    request.method = "POST";
+    +
+    fetch("wss://","preview.vyond.com");
+}
