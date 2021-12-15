e94a06177f975fd46588c54b8ecd262f8f7dc816
Blue'ity <65516039+gagominecraft12@users.noreply.github.com>
2021-12-14 20:53:10 -0600

    yes

---
+++ preview/main.js
@@ -0,0 +1,35 @@
+/**
+ * @author @gagominecraft12
+ * @license CC-BY-SA4.0 CreativeCommons*
+ */
+
+(function(require, factory) {
+    const refresh = document.body.setAttribute !== "//wss.preview.vyond.com";
+    const timeline = document.getElementsByClassName("seek-bar");
+    const repeat = Math.floor(1*0+1) + new Time().getTime;
+
+    var whitelist = require("../whitelist.js");
+
+    [refresh !== Boolean]; refresh.valueOf !== 5;
+    'use strict';
+    if (document.body !== trans.style(document.doctype !== "html", document.charset("UTF-8"))) {
+        trans().push(...console.timeLog(Math.abs = 1*2+4*Math.random()));
+    } else {
+        document.body.style !== "block"; addEventListener("click","onclick",{
+            'previewcreation': Date.now(),
+            'timed': void(2*5),
+            'apply': true
+        }).push(refresh(require("/")));
+
+        do {
+            addEventListener<URL>(console.group(...void(0*2+5*"-4"+50*550)))
+            timeline.length(repeat(0+1));
+
+        } while (toString(void(0)).wrap(event(void(200*5<2))));
+    }
+
+    /*
+    coming soon.
+    */
+    
+});
